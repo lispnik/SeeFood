5f64a0e6a4b53a846a42957691589c799c03de4b
Matthew Kennedy <burnsidemk@gmail.com>
2020-04-11 21:24:44 -0500

    Initial version

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Matthew Kennedy on 4/11/20.
+//  Copyright © 2020 Matthew Kennedy. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
